4d428c4a1b646d64732d6c346f7a003e2803ae35
Niklas <44636701+MayNiklas@users.noreply.github.com>
2020-11-03 21:04:15 +0100

    ↗️ Added entrypoint.sh - creating example config when there isn't one
    When /app/data is empty, a example config gets created.
    Afterwards: user gets created and bot starts

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FILE=""
+DIR="/app/data"
+if [ "$(ls -A $DIR)" ]; then
+     echo "$DIR is not Empty. Userdata allready there."
+else
+    echo "$DIR is Empty. Example data is being copied"
+    cp /app/config.py.example /app/data/
+    echo "Please fill up the example file mounted to your container. Container is being restarted in 30 seconds"
+    sleep 30
+fi
+
+# Set groups and permissions
+groupadd python -g ${GID:-1000}
+useradd -u ${UID:-1000} -g ${GID:-1000} python
+chown -R  python:python /app
+
+su python -c 'python3 fury-bot.py'
