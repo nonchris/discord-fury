224a754eabaa01ea144fd364f52166ce2a710efa
nonchris <git@chris-ge.de>
2021-03-06 00:31:58 +0100

    :arrow_upper_right: Added send embed function from help command
    This is a redundant copy of the send_embed function found in help.py but I didn't want to wreak havoc by removing it from help. This will follow an other day :wink:

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,29 @@
-# built in
 import re
 from typing import Tuple, Union, List
+
 # pip
 import discord
-from discord.ext import commands
+from discord.errors import Forbidden
+
+
+async def send_embed(ctx, embed):
+    """
+    Function that handles the sending of embeds
+    -> Takes context and embed to send
+    - tries to send embed in channel
+    - tries to send normal message when that fails
+    - tries to send embed private with information abot missing permissions
+    If this all fails: https://youtu.be/dQw4w9WgXcQ
+    """
+    try:
+        await ctx.send(embed=embed)
+    except Forbidden:
+        try:
+            await ctx.send("Hey, seems like I can't send embeds. Please check my permissions :)")
+        except Forbidden:
+            await ctx.author.send(
+                f"Hey, seems like I can't send any message in {ctx.channel.name} on {ctx.guild.name}\n"
+                f"May you inform the server team about this issue? :slight_smile:", embed=embed)
 
 
 def get_rid(ctx, role_input: Union[list, Tuple]) -> Tuple[List[int], int]:
