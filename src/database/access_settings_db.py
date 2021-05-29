a047e22cf4342ec14c66052affec9833ee2253db
nonchris <git@chris-ge.de>
2021-05-30 00:31:24 +0200

    Add module to handle accesses to settings table
    This module was written by me for an other bot and is now ported to the here used settings table.
    It contains some functions to make selections, additions and deletions easier.

---
+++ src/database/access_settings_db.py
@@ -0,0 +1,99 @@
+"""
+Written by:
+https://github.com/nonchris/
+"""
+
+import logging
+from datetime import datetime
+from typing import Union, List
+
+from sqlalchemy import select, and_, delete
+
+import database.db_models as db
+
+logger = logging.getLogger('my-bot')
+
+
+def get_settings_for(guild_id: int, setting: str, session=db.open_session()) -> Union[List[db.Settings], None]:
+    """
+    Searches db for setting in a guild that matches the setting name
+
+    :param guild_id: id of the guild to search for
+    :param setting: name of the setting to search for
+    :param session: session to search with, helpful if object shall be edited, since the same session is needed for this
+
+    :return: list of settings that match the given given setting name
+    """
+
+    sel_statement = select(db.Settings).where(
+        and_(
+            db.Settings.guild_id == guild_id,
+            db.Settings.setting == setting
+        )
+    )
+    entries = session.execute(sel_statement).all()
+    return [entry[0] for entry in entries] if entries else None
+
+
+def get_setting(guild_id: int, setting: str, value: str,
+                session=db.open_session()) -> Union[db.Settings, None]:
+    """
+    Searches db for one specific setting and returns if if exists
+
+    :param guild_id: id of the guild to search for
+    :param value: value of the setting to search for
+    :param setting: name of the setting to search for
+    :param session: session to search with, helpful if object shall be edited, since the same session is needed for this.
+
+    :return: database entry if exists with those specific parameters, else None
+    """
+
+    sel_statement = select(db.Settings).where(
+        and_(
+            db.Settings.guild_id == guild_id,
+            db.Settings.setting == setting,
+            db.Settings.value == value
+        )
+    )
+    entry = session.execute(sel_statement).first()
+    return entry[0] if entry else None
+
+
+def add_setting(guild_id: int, setting: str, value: str,
+                active=True, set_by="", set_date=datetime.now(), session=db.open_session()):
+    """
+    Add an entry to the settings database
+
+    :param guild_id: id the setting is in
+    :param value: value of the setting - probably name of a word-list
+    :param set_by: user id or name of the member who entered that setting - could be neat for logs
+    :param set_date: date the setting was configured
+    :param setting: setting type to add
+    :param active: if setting shall be active, not used at the moment
+    :param session: session to search with, helpful if object shall be edited, since the same session is needed for this
+    """
+    entry = db.Settings(guild_id=guild_id, setting=setting, value=value,
+                        active=active, set_by=set_by, set_date=set_date)
+    session.add(entry)
+    session.commit()
+
+
+def del_setting(guild_id: int, setting: str, value: str, session=db.open_session()):
+    """
+    Delete an entry from the settings table
+
+    :param guild_id: id the setting is in
+    :param value: value of the setting - probably name of a word-list
+    :param setting: setting type to delete
+    :param session: session to search with, helpful if object shall be edited, since the same session is needed for this
+    """
+
+    statement = delete(db.Settings).where(
+        and_(
+            db.Settings.guild_id == guild_id,
+            db.Settings.setting == setting,
+            db.Settings.value == value
+        )
+    )
+    session.execute(statement)
+    session.commit()
