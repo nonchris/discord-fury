66285309b87ab95ca890e438f7c3f5aa81d87353
nonchris <git@chris-ge.de>
2021-05-29 23:22:17 +0200

    Remove check and copy of config.py since module is no longer used

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-FILE=""
-DIR="/app/data"
-if [ "$(ls -A $DIR)" ]; then
-     echo "$DIR is not Empty. Userdata allready there."
-else
-    echo "$DIR is Empty. Example data is being copied"
-    cp /app/config.py.example /app/data/config.py
-    echo "Please fill up the example file mounted to your container. Container is being restarted in 30 seconds"
-    sleep 30
-fi
 
 # Set groups and permissions
 groupadd python -g ${GID:-1000}
