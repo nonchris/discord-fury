df31668452c02bcdc01df3e7b478bb2da058deae
nonchris <git@chris-ge.de>
2021-06-28 22:35:56 +0200

    Rewrite command to track a new channel use the new database

--- src/cogs/set_settings.py
+++ src/cogs/set_settings.py
@@ -1,6 +1,8 @@
 # built in
 import os
 import time
+from typing import Union
+
 import sql_utils as sqltils
 
 # pip
@@ -9,7 +11,10 @@ from discord.ext import commands
 
 # own files
 from environment import PREFIX, CHANNEL_TRACK_LIMIT
-import utils
+import database.db_models as db_models
+import database.access_settings_db as settings_db
+import database.access_channels_db as channels_db
+import utils as utils
 
 global db_file
 db_file = "data/fury1.db"  # variables will be gone with the next update 
@@ -24,53 +29,100 @@ class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="set-voice-channel", aliases=["set-voice", "svc"],
-                      help=f"Allows you to register a voice channel that members can join into to get an own channel\n\n \
-        Usage: \
-        `{PREFIX}svc` [_pub_ | _priv_] [_channel-id_]\n\n \
-        _pub_ and _priv_ are the options for the channels that are created when joining the creation-channel.\n \
-        This option is - obviously - admin only")
+    @commands.command(name="svc", aliases=["set-voice", "set-voice-channel"],
+                      help=f"Register a voice channel that members can join to get an own channel\n\n"
+        "__Usage:__\n"
+        f"`{PREFIX}svc` [_public_ | _private_] [_channel-id_]\n\n"
+        "_public_ or _private_ is the option for the channel-type that is created when joining the tracked-channel.\n"
+        "This option is - obviously - admin only\n\n"
+        f"Aliases: `{PREFIX}set-voice [channel type] [channel id]`")
     @commands.has_permissions(administrator=True)
-    async def set_voice(self, ctx, setting: str, value: str):
-        # possible settings switch -returns same value but nothing if key isn't valid
+    async def set_voice(self, ctx: commands.Context, setting: str, value: str):
+        """
+        Add a voice channel setting to the database
+        :param ctx: command context
+        :param setting: setting to be added like 'public'
+        :param value: id or mention the channel to be entered into the database
+        """
+
+        # channel to be returned with dict
+        # cache variable - the function would be called for each key if we'd place it in each tuple of the dict
+        _channel = utils.get_chan(ctx.guild, value)
+
+        # 'translation' dict
+        # used to verify that input is correct and to make sure that we always handle the same name internally
         settings = {
-            "pub-channel": ("pub", utils.get_chan(ctx.guild, value)),
-            "pub": ("pub", utils.get_chan(ctx.guild, value)),
-            "priv-channel": ("priv", utils.get_chan(ctx.guild, value)),
-            "priv": ("priv", utils.get_chan(ctx.guild, value)),
+            "pub-channel": ("public_channel", _channel),
+            "public-channel": ("public_channel", _channel),
+            "pub": ("public_channel", _channel),
+            "public": ("public_channel", _channel),
+
+            "priv-channel": ("private_channel", _channel),
+            "private-channel": ("private_channel", _channel),
+            "priv": ("private_channel", _channel),
+            "private": ("private_channel", _channel),
         }
+
         # trying to get a corresponding channel / id
-        setting, value = settings.get(setting)
-        # if value is "None" this means that there is no such setting or no such value for it
+        setting, channel = settings[setting]
+
+        # if channel is "None" this means that there is no such setting or no such channel for it
         # -> ensures that the process of getting a correct setting has worked
-        if value is not None and value.type == discord.ChannelType.voice:
-
-            # connecting to db - creating a new one if there is none yet
-            db = sqltils.DbConn(db_file, ctx.guild.id, "setting")
-
-            # Settings won't be stored if max watched channels are reached
-            # -> searching for amount of matching entries
-            if len(db.search_table(value=setting, column="setting")) >= CHANNEL_TRACK_LIMIT:
-
-                text = f"Hey, you can't make me watch more than {CHANNEL_TRACK_LIMIT} channels for this setting\n \
-                        If you wanna change the channels I watch use `{PREFIX}ds [channel-id]` to remove a channel from your settings"
-                emby = utils.make_embed(color=discord.Color.orange(), name="Too many entries", value=text)
-                await ctx.send(embed=emby)
-
-            # writing entry to db - the way things sould go
-            else:
-                entry = (setting, "value_name", value.id, time.strftime("%Y-%m-%d %H:%M:%S"), SQL_VERSION)
-                db.write_server_table(entry)
-
-                emby = utils.make_embed(color=discord.Color.green(), name="Success", value="Setting saved")
-                await ctx.send(embed=emby)
-
-        # when false inputs were given
-        else:
-            value = ("Please ensure that you've entered a valid setting \
-                    and channel-id for that setting.")
-            emby = utils.make_embed(color=discord.Color.orange(), name="Can't get setting", value=value)
+        if channel is None:
+            emby = utils.make_embed(
+                color=discord.Color.orange(),
+                name="Can't get setting",
+                value="Please ensure that you've entered a valid setting and channel-id for that setting.")
             await ctx.send(embed=emby)
+            return
+
+        # if channel type is not voice channel
+        if type(channel) is not discord.VoiceChannel:
+            embed = utils.make_embed(
+                name='Not a voice channel',
+                value=f"The channel {channel.name} is no voice channel, please enter a valid channel-id",
+                color=utils.yellow
+            )
+            await ctx.send(embed=embed)
+            return
+
+        # Settings won't be stored if max watched channels are reached
+        # -> searching for amount of matching entries
+        entries = settings_db.get_all_settings_for(ctx.guild.id, setting)
+        if entries and len(entries) >= CHANNEL_TRACK_LIMIT:
+            emby = utils.make_embed(
+                color=utils.orange, name="Too many entries",
+                value=f"Hey, you can't make me watch more than {CHANNEL_TRACK_LIMIT} channels for this setting\n"
+                      f"If you wanna change the channels I watch use `{PREFIX}ds [channel-id]` "
+                      f"to remove a channel from your settings")
+
+            await ctx.send(embed=emby)
+            return
+
+        # check if channel was already given to track
+        session = db_models.open_session()
+        entry: Union[db_models.Settings, None] = settings_db.get_setting_by_value(ctx.guild.id, channel.id, session)
+
+        # if channel is already registered - update
+        if entry:
+            entry.setting = setting
+
+            session.add(entry)
+            session.commit()
+
+        # create new entry, channel not tracked yet
+        else:
+            # write entry to db
+            settings_db.add_setting(
+                guild_id=ctx.guild.id,
+                setting=setting,
+                value=channel.id,
+                set_by=ctx.author.id,
+            )
+
+        emby = utils.make_embed(
+            color=utils.green, name="Success", value=f"Set {channel.name} as {setting.replace('_', ' ')}")
+        await ctx.send(embed=emby)
 
     @commands.command(name="get-settings", aliases=["gs"], help=f"\
                                 Get a list of all watched 'create-voice' channels - short: `{PREFIX}gs`")
